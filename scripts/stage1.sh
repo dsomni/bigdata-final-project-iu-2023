161c6d6ac738a8fbad9a8dfff9af95fa2be2c699
Kadaverciant <kadaverciant@gmail.com>
2023-04-09 14:09:59 +0000

    Add [save avro data on hdfs using sqoop] stage 1

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -6,6 +6,16 @@
 
 # psql -U postgres -d project -f sql/db.sql
 
+hdfs dfs -rm -r /project
 
+sqoop import-all-tables \
+    -Dmapreduce.job.user.classpath.first=true \
+    --connect jdbc:postgresql://localhost/project \
+    --username postgres \
+    --warehouse-dir /project \
+    --as-avrodatafile \
+    --compression-codec=snappy \
+    --outdir /project/avsc \
+    --m 1
 
 
